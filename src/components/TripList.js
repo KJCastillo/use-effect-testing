cc9b85fd0c5282b033ef98915b442352c7d325f4
Kevin Castillo <kevincastillo318@gmail.com>
2022-08-15 17:27:04 -0700

    filter json, dependency array

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -3,14 +3,15 @@ import './TripList.css'
 
 export default function TripList() {
   const [trips, setTrips] = useState([]);
+  const [url, setUrl] = useState("http://localhost:3000/trips")
 
   console.log(trips);
 
   useEffect(() => {
-    fetch("http://localhost:3000/trips")
+    fetch(url)
       .then((response) => response.json())
       .then((json) => setTrips(json));
-  }, []);
+  }, [url]);
 
   console.log(trips);
 
@@ -25,6 +26,10 @@ export default function TripList() {
           </li>
         ))}
       </ul>
+      <div className="filters">
+        <button onClick={() => setUrl("http://localhost:3000/trips?loc=europe")}>European Trips</button>
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>All Trips</button>
+      </div>
     </div>
   );
 }
